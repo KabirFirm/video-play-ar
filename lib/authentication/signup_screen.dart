50ca22a06fe3aad4a1a44ff5b8706d89b6c7305c
Humayun Kabir <humayun.kabir@egeneration.co>
2023-08-05 16:55:05 +0600

    validation added in signup screen

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -21,6 +21,7 @@ class _SignupScreenState extends State<SignupScreen> {
   TextEditingController userNameTextEditingController = TextEditingController();
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+  GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
 
   var authenticationController = AuthenticationController.instanceAuth;
 
@@ -29,173 +30,184 @@ class _SignupScreenState extends State<SignupScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100.0,
-              ),
-
-              Text(
-                'Signup Screen',
-                style: GoogleFonts.akayaKanadaka(
-                    fontSize: 35.0,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.bold
+          child: Form(
+            key: signupFormKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 100.0,
                 ),
-              ),
-              const SizedBox(height: 30.0,),
-              Text(
-                'This is prepared by Humayun Kabir',
-                style: GoogleFonts.akayaKanadaka(
-                    fontSize: 15.0,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.normal
-                ),
-              ),
-              const SizedBox(height: 30.0,),
-              // username textfield
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: InputTextWidget(
-                    textEditingController: userNameTextEditingController,
-                    labelString: 'Username',
-                    iconData: Icons.person_outline,
-                    isObscure: false
-                ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              // email textfield
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: InputTextWidget(
-                    textEditingController: emailTextEditingController,
-                    labelString: 'Email',
-                    iconData: Icons.email_outlined,
-                    isObscure: false
-                ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
 
-              // password textfield
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: InputTextWidget(
-                    textEditingController: passwordTextEditingController,
-                    labelString: 'Password',
-                    iconData: Icons.lock_outline,
-                    isObscure: true
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              // signup button
-              showProgressBar == false ?
-              Column(
-                // todo: a) remove column
-                children: [
-                  // signup button
-                  Container(
-                    width: MediaQuery.of(context).size.width - 38,
-                    height: 54,
-                    decoration: const BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(10)
-                        )
-                    ),
-                    child: InkWell(
-                      onTap: () {
-
-                        //todo - form validation change, use form
-                        if(userNameTextEditingController.text.isNotEmpty
-                            && emailTextEditingController.text.isNotEmpty
-                            && passwordTextEditingController.text.isNotEmpty)
-                          {
-                            // show progressBar
-                            setState(() {
-                              showProgressBar = true;
-                            });
-                            // hide keyboard
-                            hideKeyboard();
-                            // create account for user
-                            authenticationController.createAccount(
-                                userNameTextEditingController.text,
-                                emailTextEditingController.text,
-                                passwordTextEditingController.text
-                            );
-
-
-                          }
-                      },
-                      child: const Center(
-                          child: Text(
-                            'Signup',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500
-                            ),
-                          )),
-                    ),
+                Text(
+                  'Signup Screen',
+                  style: GoogleFonts.akayaKanadaka(
+                      fontSize: 35.0,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold
                   ),
+                ),
+                const SizedBox(height: 30.0,),
+                Text(
+                  'This is prepared by Humayun Kabir',
+                  style: GoogleFonts.akayaKanadaka(
+                      fontSize: 15.0,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.normal
+                  ),
+                ),
+                const SizedBox(height: 30.0,),
+                // username textfield
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: InputTextWidget(
+                      textEditingController: userNameTextEditingController,
+                      labelString: 'Username',
+                      iconData: Icons.person_outline,
+                      isObscure: false
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                // email textfield
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: InputTextWidget(
+                      textEditingController: emailTextEditingController,
+                      labelString: 'Email',
+                      iconData: Icons.email_outlined,
+                      isObscure: false,
+                    isEmail: true,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
 
-                  const SizedBox(height: 15,),
-                  // not have an account text and button
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Already have an account ? ',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey
-                        ),
+                // password textfield
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: InputTextWidget(
+                      textEditingController: passwordTextEditingController,
+                      labelString: 'Password',
+                      iconData: Icons.lock_outline,
+                      isObscure: true
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 20.0,
+                ),
+
+                // signup button
+                showProgressBar == false ?
+                Column(
+                  // todo: a) remove column
+                  children: [
+                    // signup button
+                    Container(
+                      width: MediaQuery.of(context).size.width - 38,
+                      height: 54,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(10)
+                          )
                       ),
-                      InkWell(
-                        onTap: ()
-                        {
-                          // send user to login page
-                          Get.to(const LoginScreen());
+                      child: InkWell(
+                        onTap: () {
+
+                          final form = signupFormKey.currentState;
+
+                          if(form!.validate())
+                            {
+                              // show progressBar
+                              setState(() {
+                                showProgressBar = true;
+                              });
+                              // hide keyboard
+                              hideKeyboard();
+                              // create account for user
+                              authenticationController.createAccount(
+                                  userNameTextEditingController.text,
+                                  emailTextEditingController.text,
+                                  passwordTextEditingController.text
+                              );
+                            }
+
+                          /*//todo - form validation change, use form
+                          if(userNameTextEditingController.text.isNotEmpty
+                              && emailTextEditingController.text.isNotEmpty
+                              && passwordTextEditingController.text.isNotEmpty)
+                            {
+
+
+
+                            }*/
                         },
-                        child: const Text(
-                          'Login Now',
+                        child: const Center(
+                            child: Text(
+                              'Signup',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            )),
+                      ),
+                    ),
+
+                    const SizedBox(height: 15,),
+                    // not have an account text and button
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Already have an account ? ',
                           style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue
+                              fontSize: 16,
+                              color: Colors.grey
                           ),
                         ),
-                      )
+                        InkWell(
+                          onTap: ()
+                          {
+                            // send user to login page
+                            Get.to(const LoginScreen());
+                          },
+                          child: const Text(
+                            'Login Now',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue
+                            ),
+                          ),
+                        )
 
-                    ],
-                  ),
-                ],
-              ) : Container(
-                // show animation
-                child: const SimpleCircularProgressBar(
-                  progressColors: [
-                    Colors.green,
-                    Colors.blueAccent,
-                    Colors.red,
-                    Colors.amber,
-                    Colors.purpleAccent
+                      ],
+                    ),
                   ],
-                  animationDuration: 3,
-                  backColor: Colors.white,
+                ) : Container(
+                  // show animation
+                  child: const SimpleCircularProgressBar(
+                    progressColors: [
+                      Colors.green,
+                      Colors.blueAccent,
+                      Colors.red,
+                      Colors.amber,
+                      Colors.purpleAccent
+                    ],
+                    animationDuration: 3,
+                    backColor: Colors.white,
+                  ),
                 ),
-              ),
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/input_text_widget.dart
+++ lib/widgets/input_text_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/src/get_utils/get_utils.dart';
 
 class InputTextWidget extends StatelessWidget {
 
@@ -8,6 +9,7 @@ class InputTextWidget extends StatelessWidget {
   final String labelString;
   final bool isObscure;
   final bool? isEnable;
+  final bool? isEmail;
 
   InputTextWidget(
   {
@@ -16,14 +18,15 @@ class InputTextWidget extends StatelessWidget {
     this.assetReference,
     required this.labelString,
     required this.isObscure,
-    this.isEnable
+    this.isEnable,
+    this.isEmail
 
 });
   @override
   Widget build(BuildContext context) {
 
     // todo: a) add validation, b) error color c) email validation
-    return TextField(
+    return TextFormField(
       controller: textEditingController,
       enabled: isEnable ?? true,
       decoration: InputDecoration(
@@ -54,8 +57,32 @@ class InputTextWidget extends StatelessWidget {
               color: Colors.grey
           ),
         ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6.0),
+          borderSide: const BorderSide(
+              color: Colors.red
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6.0),
+          borderSide: const BorderSide(
+              color: Colors.red
+          ),
+        ),
       ),
       obscureText: isObscure,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return '';
+        } else {
+          if (!GetUtils.isEmail(value) && isEmail == true) {
+            return '';
+          } else {
+
+            return null;
+          }
+        }
+      },
     );
   }
 }
